70cf3b99e548c96d118635005468f48f0c7bb50f
Ron Lauren Hombre <118486316+ronhombre@users.noreply.github.com>
2024-04-09 08:22:17 +0800

    Improved build process

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,19 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     kotlin("multiplatform")
     id("maven-publish")
+    id("signing")
 }
 
 group = "asia.hombre"
 version = "0.0.2"
+
+val projectName = "keccak"
+
+val mavenDir = projectDir.resolve("maven")
+
+val isAutomated = true
 
 repositories {
     mavenCentral()
@@ -40,29 +49,28 @@ kotlin {
     }
 }
 
+signing {
+    if (project.hasProperty("signing.gnupg.keyName")) {
+        useGpgCmd()
+        sign(publishing.publications)
+    }
+}
+
 publishing {
     repositories {
-        mavenLocal()
-        /*mavenLocal {
-            url = uri("${System.getProperty("user.home")}/.m2/repository")
-        }*/
+        maven {
+            url = mavenDir.toURI()
+        }
     }
     publications {
-        create<MavenPublication>("keccak") {
-            artifactId = "keccak"
-            from(components["kotlin"])
-        }
-        getByName<MavenPublication>("kotlinMultiplatform") {
-            artifactId = "keccak-kmm"
-        }
-        getByName<MavenPublication>("js") {
-            artifactId = "keccak-js"
-        }
-        getByName<MavenPublication>("jvm") {
-            artifactId = "keccak-jvm"
-        }
-        getByName<MavenPublication>("windows") {
-            artifactId = "keccak-win"
+        //Dynamically rename all artifacts
+        this.forEach {
+            val mavenPublication = it as MavenPublication
+            mavenPublication.artifactId = projectName +
+                    if(mavenPublication.artifactId.contains("-"))
+                        "-" + mavenPublication.artifactId.split("-").last()
+                    else
+                        ""
         }
     }
     publications.withType<MavenPublication> {
@@ -94,5 +102,76 @@ publishing {
                 url.set("https://github.com/ronhombre/KeccakKotlin")
             }
         }
+    }
+}
+
+fun parseArtifactId(artifactId: String): String {
+    val list = artifactId.splitToSequence("-").map { it.replaceFirstChar(Char::uppercase) }
+
+    return list.joinToString("")
+}
+
+fun parseArtifactArchiveName(artifact: MavenPublication): String {
+    return artifact.artifactId + "-" + artifact.version + "-bundle.zip"
+}
+
+for (publication in publishing.publications.asMap) {
+    val artifact = publication.value as MavenPublication
+    val parsedArtifactId = parseArtifactId(artifact.artifactId)
+    val bundleFileName = parseArtifactArchiveName(artifact)
+
+    tasks.register<Zip>("bundle$parsedArtifactId") {
+        group = "Bundle"
+        from(mavenDir)
+        val mavenDeepDir = artifact.groupId.replace(".", "/") + "/" + artifact.artifactId
+        include("$mavenDeepDir/*/*")
+        destinationDirectory = mavenDir
+        archiveFileName = parseArtifactArchiveName(artifact)
+    }
+
+    tasks.register<Exec>("publish" + parsedArtifactId + "ToMavenCentral") {
+        group = "Publish"
+        if(!mavenDir.resolve(bundleFileName).exists())
+            throw RuntimeException("Bundle does not exist! Please run `bundle$parsedArtifactId`")
+
+        commandLine(
+            "curl", "-X", "POST",
+            "https://central.sonatype.com/api/v1/publisher/upload?name=${artifact.artifactId}&publishingType=" + if(isAutomated) "AUTOMATED" else "USER_MANAGED",
+            "-H", "accept: text/plain",
+            "-H", "Content-Type: multipart/form-data",
+            "-H", "Authorization: Bearer " + System.getenv("SONATYPE_TOKEN"),
+            "-F", "bundle=@$bundleFileName;type=application/x-zip-compressed"
+        )
+        workingDir(mavenDir.toString())
+        standardOutput = ByteArrayOutputStream()
+        errorOutput = ByteArrayOutputStream()
+
+        // Execute some action with the output
+        doLast {
+            println("$standardOutput")
+            println("$errorOutput")
+        }
+    }
+}
+
+tasks.register("bundleAll") {
+    group = "Bundle"
+    dependsOn("publish")
+
+    for (publication in publishing.publications.asMap) {
+        val artifact = publication.value as MavenPublication
+
+        dependsOn("bundle" + parseArtifactId(artifact.artifactId))
+    }
+}
+
+tasks.register("publishAllToMavenCentral") {
+    group = "Publish"
+    dependsOn("bundleAll")
+
+    for (publication in publishing.publications.asMap) {
+        val artifact = publication.value as MavenPublication
+
+        dependsOn("publish" + parseArtifactId(artifact.artifactId) + "ToMavenCentral")
     }
 }
